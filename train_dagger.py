337406b2347ef1b72c7bcd36061d59cf3212d585
amnonh-uw <amnonh@uw.edu>
2017-11-06 16:51:14 -0800

    move parsing to args.py

--- train_dagger.py
+++ train_dagger.py
@@ -1,22 +1,12 @@
 import sys
-import argparse
+from args import get_args
 import tensorflow as tf
 from block_world import BlockWorldEnv as make
 from dagger import Dagger
 from dagger_policy import DaggerPolicy
 
 def main(argv):
-    parser = argparse.ArgumentParser(description='block_world')
-    parser.add_argument('--show-obs', dest='show_obs', action='store_true')
-    parser.add_argument('--no-show-obs', dest='show_obs', action='store_false')
-    parser.add_argument('--run', dest='run', action='store_true')
-    parser.add_argument('--no-run', dest='run', action='store_false')
-    parser.add_argument('--reach-minimum', type=float, default=0.1)
-    parser.set_defaults(show_obs=False)
-    parser.set_defaults(run=True)
-    args = parser.parse_args(argv)
-
-    print(args)
+    args = get_args(argv)
 
     args.width = DaggerPolicy.width
     args.height = DaggerPolicy.height
@@ -30,12 +20,7 @@ def main(argv):
 
     dagger = Dagger(env,
                     DaggerPolicy,
-                    num_rollouts = 25,
-                    train_batch_size = 25,
-                    train_epochs = 20,
-                    iterations = 20,
-                    train_report_frequency = 10,
-                    dir_name = 'tmp_storage')
+                    **args)
 
     dagger.learn(save_file_name="dagger_block_world")
 
--- train_dagger_all.py
+++ train_dagger_all.py
@@ -1,22 +1,21 @@
 import sys
-import argparse
-import tensorflow as tf
+from args import get_args
 from block_world import BlockWorldEnv as make
 from dagger import Dagger
-from dagger_policy import DaggerPolicy
+from dagger_policy_nets import DaggerPolicy
 
 def main(argv):
     env = make(run=False)
+    
+    args = get_args(argv)
+
+    print(args)
 
     dagger = Dagger(env,
                     DaggerPolicy,
-                    num_rollouts = 25,
-                    train_batch_size = 25,
-                    train_epochs = 20,
-                    iterations = 20,
-                    train_report_frequency = 10,
-                    dir_name = 'tmp_storage')
+                   **vars(args))
 
+    # dagger.learn_all_samples(save_file_name="dagger_block_world", load_file_name="dagger_block_world")
     dagger.learn_all_samples(save_file_name="dagger_block_world")
 
     env.close()
