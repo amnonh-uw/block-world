48ac07eaf7551493b896eb041dc1fd92732ff436
amnonh-uw <amnonh@uw.edu>
2017-11-27 11:18:16 -0800

    add datadefs and policies

--- path.sh
+++ path.sh
@@ -1,1 +1,1 @@
-export PYTHONPATH=$PYTHONPATH:~/aigym:~/block_world:~/block_world/baselines:~/block_world/dagger/:~/block_world/lib:~/block_world/lib/networks
+export PYTHONPATH=$PYTHONPATH:~/aigym:~/block_world:~/block_world/baselines:~/block_world/dagger/:~/block_world/lib:~/block_world/lib/networks:~/block_world/policies:~/block_world/datadefs
