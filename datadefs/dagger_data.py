3c7a36eee7b13c0de091db6f02183fad084a0009
amnonh-uw <amnonh@uw.edu>
2017-11-29 12:50:23 -0800

    make work

--- datadefs/dagger_data.py
+++ datadefs/dagger_data.py
@@ -3,11 +3,11 @@ import sys
 
 def get_type_dict():
     type_dict = dict()
-    type_dict['leftcam'] = tf.uint8
-    type_dict['rightcam'] = tf.uint8
-    type_dict['centercam'] = tf.uint8
-    type_dict['multichanneldepthcam'] = tf.int32
-    type_dict['normalcam'] = tf.uint8
+    type_dict['leftcam'] = "img8"
+    type_dict['rightcam'] = "img8"
+    type_dict['centercam'] = "img8"
+    type_dict['multichanneldepthcam'] = "img16"
+    type_dict['normalcam'] = "img8"
     type_dict['target_pos'] = tf.float32
     type_dict['target_rot'] = tf.float32
     type_dict['target_screen_pos'] = tf.float32
