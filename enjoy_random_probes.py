a0487163ebe7a83057c39f75fcc4148f4b1ce74e
amnonh-uw <amnonh@uw.edu>
2017-11-30 11:45:50 -0800

    add

---
+++ enjoy_random_probes.py
@@ -0,0 +1,28 @@
+import sys
+from args import get_args, env_args
+from block_world import BlockWorldEnv as make
+from dagger import Dagger
+from importlib import import_module
+
+def main(argv):
+    args = get_args(argv)
+
+    x = env_args(args)
+    print(x)
+    env = make(**vars(x))
+
+    policy_mod = import_module(args.policy_source)
+    policy_class = getattr(policy_mod, 'DaggerPolicy')
+    policy = policy_class(**vars(args))
+
+    dagger = Dagger(env,
+                    policy,
+                    **vars(args))
+
+    dagger.num_probes = 10
+    dagger.test(args.save_file_name)
+
+    env.close()
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
